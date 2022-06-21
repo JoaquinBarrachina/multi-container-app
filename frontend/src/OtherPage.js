85182bfc3eea8c3dbd0e63e598fe472820e003d8
Joaquin <joaquinbarjor@gmail.com>
2022-06-21 23:27:24 +0200

    first commit

---
+++ frontend/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DefaultPage = () => {
+  return (
+    <div>
+      Im so other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default DefaultPage;
